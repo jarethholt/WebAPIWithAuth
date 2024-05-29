e60fe4cc2ae7f043a0b76d9c6b3a4f3228e7a712
jarethholt <jareth.holt@gmail.com>
2024-05-29 21:23:04 +0200

    feat: Create Authorize attribute

---
+++ WebAPIWithAuth/Helpers/AuthorizeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WebAPIWithAuth.Models;
+
+namespace WebAPIWithAuth.Helpers;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var user = (User?)context.HttpContext.Items["User"];
+        if (user is null)
+            context.Result =
+                new JsonResult(
+                    new { message = "Unauthorized" })
+                { StatusCode = StatusCodes.Status401Unauthorized };
+    }
+}
