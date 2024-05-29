c5bc0fc032f80d68c140b08a09536f8898684ea8
jarethholt <jareth.holt@gmail.com>
2024-05-29 10:38:57 +0200

    feat: Add Hero and HeroDTO classes

---
+++ WebAPIWithAuth/Models/Hero.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPIWithAuth.Models;
+
+public class Hero
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public string LastName { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+}
---
+++ WebAPIWithAuth/Models/HeroDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPIWithAuth.Models;
+
+public class HeroDTO
+{
+    public required string FirstName { get; set; }
+    public string LastName { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+}
