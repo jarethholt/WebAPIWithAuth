53d268294dfeb0eb762ea8f82202bfc29a3e7f6e
jarethholt <jareth.holt@gmail.com>
2024-05-30 11:15:03 +0200

    feat: Add ApiKeyAttribute class

---
+++ WebAPIWithAuth/AuthHelpers/ApiKeyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebAPIWithAuth.AuthHelpers;
+
+[AttributeUsage(
+    AttributeTargets.Class | AttributeTargets.Method,
+    AllowMultiple = true,
+    Inherited = true)]
+public class ApiKeyAttribute(IConfiguration config) : Attribute, IAuthorizationFilter
+{
+    private const string _ApiKeyName = "XApiKey";
+    private readonly IConfiguration _config = config;
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var httpContext = context.HttpContext;
+        bool apiKeyPresentInHeader = httpContext.Request.Headers
+            .TryGetValue(_ApiKeyName, out var extractedApiKey);
+        var apiKey = _config[_ApiKeyName];
+
+        if ((apiKeyPresentInHeader && apiKey == extractedApiKey)
+            || httpContext.Request.Path.StartsWithSegments("/swagger"))
+            return;
+        else
+            context.Result = new UnauthorizedResult();
+    }
+}
