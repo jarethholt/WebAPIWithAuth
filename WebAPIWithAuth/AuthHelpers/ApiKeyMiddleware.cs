ae4f8eb5d5a0e897c6b3e8ee9d671d4489831326
jarethholt <jareth.holt@gmail.com>
2024-05-30 10:18:25 +0200

    feat: Add ApiKeyMiddleware

---
+++ WebAPIWithAuth/AuthHelpers/ApiKeyMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace WebAPIWithAuth.AuthHelpers;
+
+public class ApiKeyMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+    private const string _ApiKeyName = "XApiKey";
+
+    public async Task InvokeAsync(HttpContext context, IConfiguration configuration)
+    {
+        bool apiKeyPresentInHeader = context.Request.Headers
+            .TryGetValue(_ApiKeyName, out var extractedApiKey);
+        var apiKey = configuration[_ApiKeyName];
+
+        // Check that keys match OR whether we're in Swagger debug
+        if ((apiKeyPresentInHeader && apiKey == extractedApiKey)
+            || context.Request.Path.StartsWithSegments("/swagger"))
+        {
+            await _next(context);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("Invalid API key");
+        }
+    }
+}
